a78c510bc010a0ff7d7e4f2846e65300ac2d60b5
tunde-olu <tundeolu24@gmail.com>
2024-04-13 17:13:10 +0100

    testing

--- components/Confirmation/Confirmation.tsx
+++ components/Confirmation/Confirmation.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react';
 
 const Confirmation = () => {
-  return <div>Confirmation</div>;
+	return <div>successful payment</div>;
 };
 
 export default Confirmation;
